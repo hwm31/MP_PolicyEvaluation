5384c08437731fcd2a920c761622bda68589303c
Hwa <132264214+hwm31@users.noreply.github.com>
2025-04-15 10:28:27 +0900

    Add files via upload
    iOS file upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.24.5/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hwa/Library/CloudStorage/GoogleDrive-inamin@on.use.go.kr/내 드라이브/3학년/3학년 1학기/모바일프로그래밍/Team Project/PolicyEvaluation"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
